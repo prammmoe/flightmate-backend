5247de7012778c528505bba7bf9476c0639646f8
prammmoe <ikhwanpramuditha05@gmail.com>
2024-05-23 22:27:25 +0700

    add req by params to aircraft controller

--- src/routes/aircraftRoute.js
+++ src/routes/aircraftRoute.js
@@ -4,6 +4,7 @@ const aircraftController = require("../controllers/aircraftController");
 
 router.get("/aircraft", aircraftController.getAircraft);
 router.get("/aircraft/:id", aircraftController.getAircraft);
+router.get("/aircraft/:id?", aircraftController.getAircraft);
 router.post("/aircraft", aircraftController.addAircraft);
 router.put("/aircraft/:id", aircraftController.updateAircraftFull);
 router.patch("/aircraft/:id", aircraftController.updateAircraft);
