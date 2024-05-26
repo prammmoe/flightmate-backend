41bdb0e66f5454308232a92169b9ae91e7c41667
prammmoe <ikhwanpramuditha05@gmail.com>
2024-05-26 15:05:24 +0700

    fix cors and fix tunneling

--- app.js
+++ app.js
@@ -1,47 +1,38 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 9000;
-const cors = require("cors");
-
-/**
- * @package Cors
- */
 
 const corsBaseUrl = "http://localhost:3000";
+const tunnelUrl = "https://hp5znbh8-9000.asse.devtunnels.ms";
 
 app.use(
   cors({
-    origin: corsBaseUrl,
+    origin: [corsBaseUrl, tunnelUrl], // Allow both localhost and tunnel URL
     optionsSuccessStatus: 200,
   })
 );
-
-/**
- * @module Swagger
- */
 
 const swaggerUI = require("swagger-ui-express");
 const apiDocs = require("./apidocs.json");
 app.use("/apidocs", swaggerUI.serve, swaggerUI.setup(apiDocs));
 
-// Init routing
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const aircraftRoute = require("./src/routes/aircraftRoute");
 const airportRoute = require("./src/routes/airportRoute");
 const flightRoute = require("./src/routes/flightRoute");
 const bookingRoute = require("./src/routes/bookingRoute");
 
-app.use(logger("dev"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(aircraftRoute);
 app.use(airportRoute);
 app.use(flightRoute);
 app.use(bookingRoute);
 
-// Index endpoint
 app.get("/", (req, res) => {
   res.send("Pace bekasi daboy!");
 });
